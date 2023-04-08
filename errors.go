2cfc101bc14105c2d0bde622deb49b3a2792eb51
Osmany Montero <osmontero@icloud.com>
2023-04-08 13:02:40 +0300

    function to check rerrors

--- errors.go
+++ errors.go
@@ -2,9 +2,19 @@ package rerror
 
 import "strings"
 
-func Is(err error, kargs ...string) bool {
+func EIs(err error, kargs ...string) bool {
 	for _, e := range kargs {
 		if strings.Contains(err.Error(), e) {
+			return true
+		}
+	}
+	return false
+}
+
+
+func RIs(err *Error, kargs ...string) bool {
+	for _, e := range kargs {
+		if strings.Contains(err.Message, e) {
 			return true
 		}
 	}
