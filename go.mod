e6ca0beae7499a1da2ea65dbcab0986bed435a0d
Osmany Montero <osmontero@icloud.com>
2023-11-17 11:13:17 +0200

    merge holmes into rerror

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/quantfall/rerror
+module github.com/threatwinds/rerror
 
-go 1.19
+go 1.21
 
-require github.com/google/uuid v1.3.0
+require github.com/google/uuid v1.4.0
--- rerror.go
+++ rerror.go
@@ -9,6 +9,10 @@ import (
 	"github.com/google/uuid"
 )
 
+func defaultConfig() *Config {
+	return &Config{Format: "json", Level: 400}
+}
+
 type Log struct {
 	Timestamp string `json:"timestamp"`
 	Severity  string `json:"severity"`
@@ -21,49 +25,100 @@ type Error struct {
 	UUID    string `json:"uuid"`
 	Status  int    `json:"status"`
 	Message string `json:"message"`
-	Tag     string `json:"tag"`
 }
 
-func LogF(print bool, status int, tag, format string, args ...any) *Log {
+type Logger struct{
+	cnf *Config
+}
+
+type Config struct {
+	Format string // json, text
+	Level int // 100: DEBUG, 200: INFO, 300: NOTICE, 400: WARNING, 500: ERROR, 502: CRITICAL, 505: ALERT
+}
+
+func New(config *Config) *Logger{
+	var logger = new(Logger)
+	if config != nil {
+		if config.Format == "" {
+			config.Format = defaultConfig().Format
+		}
+
+		if config.Level == 0 {
+			config.Level = defaultConfig().Level
+		}
+	} else {
+		config = defaultConfig()
+	}
+
+	logger.cnf = config
+	
+	return logger
+}
+
+func (l Log) ToJSON() string {
+	b, _ := json.Marshal(l)
+	return string(b)
+}
+
+func (l Log) ToString() string {
+	return fmt.Sprintf("%s %s %s %s %d %s", l.Timestamp, l.Severity, l.UUID, l.Path, l.Line, l.Message)
+}
+
+func (l Logger) LogF(statusCode int, format string, args ...any) *Log {
 	var log = new(Log)
 
 	var severity string
 
-	if status < 400 {
+	if statusCode >= 100 && statusCode < 200 {
+		severity = "DEBUG"
+	} else if statusCode >= 200 && statusCode < 300 {
 		severity = "INFO"
-	} else if status >= 400 && status < 500 {
+	} else if statusCode >= 300 && statusCode < 400 {
+		severity = "NOTICE"
+	} else if statusCode >= 400 && statusCode < 500 {
 		severity = "WARNING"
-	} else if status >= 500 && status < 600 {
+	} else if statusCode >= 500 && statusCode < 502 {
 		severity = "ERROR"
-	} else {
+	} else if statusCode >= 502 && statusCode < 505 {
 		severity = "CRITICAL"
+	} else if statusCode >= 505 && statusCode < 510 {
+		severity = "ALERT"
+	} else {
+		severity = "DEFAULT"
 	}
 
 	_, path, line, _ := runtime.Caller(2)
 
 	log.UUID = uuid.NewString()
-	log.Status = status
+	log.Status = statusCode
 	log.Timestamp = time.Now().UTC().Format(time.RFC3339Nano)
 	log.Path = path
 	log.Line = line
 	log.Message = fmt.Sprintf(format, args...)
 	log.Severity = severity
-	log.Tag = tag
 
-	if print {
-		j, _ := json.Marshal(log)
-		fmt.Println(string(j))
+	if statusCode >=  l.cnf.Level{
+		var final string
+		switch l.cnf.Format {
+			case "json":
+				final = log.ToJSON()
+			case "text":{
+				final = log.ToString()
+			}
+		}
+
+		fmt.Println(final)
 	}
 
 	return log
 }
 
-func ErrorF(print bool, status int, tag, format string, args ...any) *Error {
-	var l = LogF(print, status, tag, format, args...)
+func (l Logger) ErrorF(statusCode int, format string, args ...any) *Error {
+	var log = l.LogF(statusCode, format, args...)
 
-	if l.Status >= 500 {
-		return &Error{UUID: l.UUID, Status: status, Message: "internal error", Tag: l.Tag}
+	if log.Status >= 500 {
+		return &Error{UUID: log.UUID, Status: statusCode, Message: "internal error"}
 	}
 
-	return &Error{UUID: l.UUID, Status: status, Message: l.Message, Tag: l.Tag}
+	return &Error{UUID: log.UUID, Status: statusCode, Message: log.Message}
 }
