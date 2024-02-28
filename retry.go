73750f9991e1cecad63749b480d2adc0cea62662
Osmany Montero <osmontero@icloud.com>
2024-02-28 12:22:52 +0200

    removing retries

--- retry.go
+++ retry.go
@@ -33,11 +33,9 @@ func (l *Logger) RunWithRetries(f func() error, exception ...string) *Error {
 
 // RunWithInfRetries executes a function and retries infinitely if any error.
 func (l *Logger) RunWithInfRetries(f func() error, exception ...string) *Error {
-	var retries = 0
 	for {
 		err := f()
 		if err != nil {
-			retries++
 			
 			e := l.ErrorF(err.Error())
 
