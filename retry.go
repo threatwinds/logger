b276c372cd2d7c168d978fb8086efe3462e1419d
Osmany Montero <osmontero@icloud.com>
2024-02-28 12:22:25 +0200

    new func to retry infinitely

--- retry.go
+++ retry.go
@@ -30,3 +30,26 @@ func (l *Logger) RunWithRetries(f func() error, exception ...string) *Error {
 		}
 	}
 }
+
+// RunWithInfRetries executes a function and retries infinitely if any error.
+func (l *Logger) RunWithInfRetries(f func() error, exception ...string) *Error {
+	var retries = 0
+	for {
+		err := f()
+		if err != nil {
+			retries++
+			
+			e := l.ErrorF(err.Error())
+
+			for _, ex := range exception {
+				if strings.Contains(err.Error(), ex) {
+					return e
+				}
+			}
+
+			time.Sleep(l.cnf.Wait)
+		} else {
+			return nil
+		}
+	}
+}
